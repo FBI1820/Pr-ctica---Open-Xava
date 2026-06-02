ff0ce1838fe2d123aaca073d84fe1edf681983f8
fabiola-lanuza <fslanuza@uamv.edu.ni>
2026-06-01 22:06:29 -0600

    leccion 4

--- src/main/java/ni/edu/uam/facturacion/modelo/Cliente.java
+++ src/main/java/ni/edu/uam/facturacion/modelo/Cliente.java
@@ -5,7 +5,8 @@ import org.openxava.annotations.*;
 import lombok.*;
 
 @Entity  // Esto marca la clase Cliente como una entidad
-@Getter @Setter // Esto hace los campos a continuación públicamente accesibles
+@Getter @Setter
+@View (name= "Simple", members = "numero, nombre") // Esto hace los campos a continuación públicamente accesibles
 public class Cliente {
 
     @Id  // La propiedad numero es la clave.  Las claves son obligatorias (required) por defecto
@@ -15,5 +16,8 @@ public class Cliente {
     @Column(length=50) // La longitud de columna se usa a nivel UI y a nivel DB
     @Required  // Se mostrará un error de validación si la propiedad nombre se deja en blanco
     String nombre;
+
+    @Embedded @NoFrame
+    Direccion direccion;
 
 }
